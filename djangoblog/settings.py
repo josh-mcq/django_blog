cd87594212832b7d9aae5cc169f427dd36b772c0
josh <joshua.g.mcquiston@gmail.com>
2015-05-18 18:31:43 -0700

    changed db back

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -58,15 +58,14 @@ WSGI_APPLICATION = 'djangoblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
-DATABASES['default'] =  dj_database_url.config()
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
