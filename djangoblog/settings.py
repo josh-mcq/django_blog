27d15c21b2c7e2a96eb18c559794abe52f56bb95
josh <joshua.g.mcquiston@gmail.com>
2015-05-22 09:18:20 -0700

    added back static_root

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -108,9 +108,9 @@ ALLOWED_HOSTS = ['*']
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-#STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/blog/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
